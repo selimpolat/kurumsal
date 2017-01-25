a31da3df2703aed87a1030fb6f715a793faa54d6
selimpolat <selim.polatt1@outlook.com>
2017-01-25 09:23:44 +0200

    master

---
+++ KurumsalWebSitesi/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KurumsalWebSitesi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Bizimle İlgili bilgileri burdan bulabilirsiniz.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+    }
+}
